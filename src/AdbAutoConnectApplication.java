cf05e788f4ab232584a5a2cae5bdc969211355dc
wanganqing <249736701@qq.com>
2023-02-24 16:09:35 +0800

    格式化

--- src/AdbAutoConnectApplication.java
+++ src/AdbAutoConnectApplication.java
@@ -6,7 +6,6 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
